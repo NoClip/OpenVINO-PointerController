37398bec7e02d049bc01b7dc7ada78e5678640c7
Bassam ElShafie <bassam.mh@gmail.com>
2020-08-16 19:11:52 +0200

    feat: gaze estimation  model, inhertied from ModelBase

--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -1,48 +1,17 @@
-'''
-This is a sample class for a model. You may choose to use it as-is or make any changes to it.
-This has been provided just to give you an idea of how to structure your model class.
-'''
+from model import ModelBase
 
-#models\gaze-estimation-adas-0002\FP16\gaze-estimation-adas-0002
 
-class Model_X:
+class GazeModel(ModelBase):
     '''
-    Class for the Face Detection Model.
+    Class for the Gaze direction estimation Model.
     '''
-    def __init__(self, model_name, device='CPU', extensions=None):
-        '''
-        TODO: Use this to set your instance variables.
-        '''
-        raise NotImplementedError
 
-    def load_model(self):
-        '''
-        TODO: You will need to complete this method.
-        This method is for loading the model to the device specified by the user.
-        If your model requires any Plugins, this is where you can load them.
-        '''
-        raise NotImplementedError
+    def preprocess_output(self, outputs, inputs):
+        # The net outputs a blob (gaze_vector) with the shape: [1, 3],
+        #   containing Cartesian coordinates of gaze direction vector.
 
-    def predict(self, image):
-        '''
-        TODO: You will need to complete this method.
-        This method is meant for running predictions on the input image.
-        '''
-        raise NotImplementedError
+        gaze_output = outputs[self.output_names[0]].buffer[0]
 
-    def check_model(self):
-        raise NotImplementedError
+        mouse_coords = (gaze_output[0], gaze_output[1])
 
-    def preprocess_input(self, image):
-    '''
-    Before feeding the data into the model for inference,
-    you might have to preprocess it. This function is where you can do that.
-    '''
-        raise NotImplementedError
-
-    def preprocess_output(self, outputs):
-    '''
-    Before feeding the output of this model to the next model,
-    you might have to preprocess the output. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        return mouse_coords, gaze_output
