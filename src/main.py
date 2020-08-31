69178741928ca757e6dbc718d9413c54f6167394
Bassam ElShafie <bassam.mh@gmail.com>
2020-08-31 15:07:48 +0200

    fix: fixed a bug where extensions were not passed to models classes, feat: added draw_output to draw eyes coords, head pose and gaze outputs, added exception handler to the feed loop

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ def start_infer(args):
     landmarks_model = args.landmarksmodel
 
     device = args.device
+    extensions = args.cpu_extension
     input_type = args.inputtype.lower()
     input_file = args.inputfile
     threshold = args.threshold
@@ -40,18 +41,18 @@ def start_infer(args):
         feed = InputFeeder(input_type="video", input_file=input_file)
 
     face_network = FaceDetectionModel(
-        model_name=face_model, device=device, threshold=threshold
-    )
+        model_name=face_model, device=device, threshold=threshold, extensions=extensions)
 
     head_network = HeadPoseModel(
-        model_name=head_model, device=device, threshold=threshold
-    )
+        model_name=head_model, device=device, threshold=threshold, extensions=extensions)
 
     landmarks_network = FacialLandmarksModel(
-        model_name=landmarks_model, device=device, threshold=threshold
-    )
+        model_name=landmarks_model,
+        device=device,
+        threshold=threshold,
+        extensions=extensions)
 
-    gaze_network = GazeModel(model_name=gaze_model, device=device, threshold=threshold)
+    gaze_network = GazeModel(model_name=gaze_model, device=device, threshold=threshold, extensions=extensions)
 
     mouse_control = MouseController("medium", "fast")
 
@@ -62,35 +63,44 @@ def start_infer(args):
 
     feed.load_data()
 
-    for flag, frame in feed.next_batch():
-        if not flag:
-            break
-
-        if not benchmark:
-            key_pressed = cv2.waitKey(60)
-
-        face_output, cropped_face_frame = face_network.predict([frame])
-        head_output, cropped_face_frame = head_network.predict([cropped_face_frame])
-        landmarks_output, cropped_eyes = landmarks_network.predict([cropped_face_frame])
-        mouse_coords, gaze_output = gaze_network.predict([head_output, cropped_eyes[0], cropped_eyes[1]])
-
-        # disable preview and mouse control while benchmarking
-        # to make it more accurate
-        if not benchmark:
-            # Input user from preview argument
-            if preview:
-                cv2.imshow("preview", cropped_face_frame)
-
-            # using a dummy try/catch to prevent PyAutoGUI fail-safe messeges 
-            # when mouse reaches the screen edge
-            try:
-                mouse_control.move(mouse_coords[0], mouse_coords[1])
-            except:
-                pass
-
-            if key_pressed == 27:
+    try:
+        for flag, frame in feed.next_batch():
+            if not flag:
                 break
-    
+
+            if not benchmark:
+                key_pressed = cv2.waitKey(60)
+
+            face_output, cropped_face_frame = face_network.predict([frame])
+            head_output, cropped_face_frame = head_network.predict([cropped_face_frame])
+            landmarks_output, cropped_eyes = landmarks_network.predict([cropped_face_frame])
+            mouse_coords, gaze_output = gaze_network.predict([head_output, cropped_eyes[0], cropped_eyes[1]])
+
+            # disable preview and mouse control while benchmarking
+            # to make it more accurate
+            if not benchmark:
+                # Input user from preview argument
+                if preview:
+                    nframe = draw_output(
+                        cropped_face_frame,
+                        head_output,
+                        landmarks_output,
+                        gaze_output,
+                    )
+                    cv2.imshow("preview", nframe)
+
+                # added pyautogui.FAILSAFE = False to mouse controller class
+                # to prevent PyAutoGUI fail-safe messeges when mouse reaches the screen edge
+                # mouse_control.move(mouse_coords[0], mouse_coords[1])
+                mouse_control.move(mouse_coords[0], mouse_coords[1])
+
+                if key_pressed == 27:
+                    break
+    except Exception as e:
+        log.error(
+            "error while predicting input source, more details as below:\n{}".format(e)
+        )
+
     # save benchmarks values to output directory
     if benchmark:
         face_network.print_benchmark()
@@ -100,6 +110,51 @@ def start_infer(args):
 
     cv2.destroyAllWindows()
     feed.close()
+
+
+def draw_output(frame, head_output, landmarks_output, gaze_output):
+    """
+    Draw bounding boxes onto the frame.
+    """
+    colors = {"BLUE": (255, 0, 0), "GREEN": (0, 255, 0), "RED": (0, 0, 255)}
+    eyes_color = colors.get("BLUE")
+    text_color = colors.get("GREEN")
+
+    left_eye = landmarks_output[0]
+    right_eye = landmarks_output[1]
+
+    # Draw eyes positions
+    cv2.rectangle(
+        frame, (left_eye[0], left_eye[1]), (left_eye[2], left_eye[3]), eyes_color, 1
+    )
+    cv2.rectangle(
+        frame, (right_eye[0], right_eye[1]), (right_eye[2], right_eye[3]), eyes_color, 1
+    )
+
+    cv2.putText(
+        frame,
+        "Pose: y:{:.2f} p:{:.2f} r:{:.2f}".format(
+            head_output[0], head_output[1], head_output[2]
+        ),
+        (10, 20),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.4,
+        text_color,
+        1,
+    )
+
+    cv2.putText(
+        frame,
+        "Gaze: {:.2f} {:.2f} {:.2f}".format(
+            gaze_output[0], gaze_output[1], gaze_output[2]
+        ),
+        (10, 40),
+        cv2.FONT_HERSHEY_COMPLEX,
+        0.5,
+        text_color,
+        1,
+    )
+    return frame
 
 def build_argparser():
     """
